435222ab47e175472c91225e834e93e74513051a
Anxious Ape <iamkhidash@gmail.com>
2023-12-18 21:29:05 +0530

    completed steps with usage of useState React Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  //? State / Variables / Hooks
+  const [step, setStep] = useState(1);
+
+  //? Styles
+  const styles = {
+    backgroundColor: "#7950f2",
+    color: "#fff",
+  };
+
+  //? Event Handlers
+  const handleNext = (e) => {
+    setStep((step) => (step < 3 ? step + 1 : step));
+  };
+
+  const handlePrevious = (e) => {
+    setStep((step) => (step > 1 ? step - 1 : step));
+  };
+
+  //? JSX Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      {/* Numbers */}
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      {/* Message */}
+      <p className="message">{`Step ${step}: ${messages[step - 1]}`}</p>
+
+      {/* Buttons */}
+      <div className="buttons">
+        <button style={styles} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={styles} onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
